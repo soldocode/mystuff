708ead10d9e2a7fa3ad42a4b41e2a4d238a63211
soldocode <riccardo.soldini@gmail.com>
2017-03-14 19:17:48 +0100

    meDXF.py modified

--- meDXF.py
+++ meDXF.py
@@ -3,14 +3,15 @@
 #*   meDXF - 2016                                                          *
 #*   Riccardo Soldini <riccardo.soldini@gmail.com>                         *
 #*                                                                         *
+#*   Last Update: 14/03/17                                                 *                      *
 #***************************************************************************
 
 
 
 import math
 from dxfwrite import DXFEngine as dxf
-from mystuff import mePath
-from mystuff import geoFun as geo
+import mePath
+import geoFun as geo
 
 
 class Drawing (object):
@@ -18,17 +19,52 @@ class Drawing (object):
                  filename=''):
         self.FileName=filename
         self.DXFDrawing=dxf.drawing(filename)
-        self.Paths=[]
+        self.Paths=[] #???
         
     
     def drawPath(self,
                  outlines=mePath.Outline(),
                  xy=[0,0],
                  layer=-1):
+        
                      
-        self.Paths.append(outlines)
+        self.Paths.append(outlines)#???
+        lay=layer
+        path=outlines.Path
+        n=outlines.Nodes
+        d=self.DXFDrawing
+        rx=xy[0]
+        ry=xy[1]
+
+        for c in path:
+         if c[0]=='line':
+            d.add(dxf.line((rx+n[c[1]][0],ry+n[c[1]][1]),
+                           (rx+n[c[2]][0],ry+n[c[2]][1]),layer=lay))
+         elif c[0]=='arc':
+            cr=geo.CircleFrom3Points(n[c[1]],n[c[3]],n[c[2]])
+            if cr['Direction']<0:
+                d.add(dxf.arc(center=(rx+cr['Center'][0],ry+cr['Center'][1]),
+                              radius=cr['Radius'],
+                              startangle=math.degrees(cr['P1Degree']),
+                              endangle=math.degrees(cr['P3Degree']),
+                              layer=lay))
+            else:    
+                d.add(dxf.arc(center=(rx+cr['Center'][0],ry+cr['Center'][1]),
+                              radius=cr['Radius'],
+                              startangle=math.degrees(cr['P3Degree']),
+                              endangle=math.degrees(cr['P1Degree']),
+                              layer=lay))
+            
+         elif c[0]=='circle':
+            rds=n[c[2]][0]-n[c[1]][0]
+            d.add(dxf.circle(rds,(rx+n[c[1]][0],ry+n[c[1]][1]),layer=lay))
         
+
+
         
+
+    def save(self):   
+        self.DXFDrawing.save()
     
     
 #******************************************************                                                  
