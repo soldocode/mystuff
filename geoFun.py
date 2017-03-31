7fe585aaa68c44e3fcc4e291bad2f54090730b08
Riccardo Soldini <soldocode@users.noreply.github.com>
2017-03-31 07:36:16 +0200

    Update geoFun.py

--- geoFun.py
+++ geoFun.py
@@ -1,5 +1,5 @@
 ##############################################################################
-#
+# riccardo.soldini@gmail.com
 #
 # Functions:
 # > DetMatrix3x3(A,B,C)
@@ -12,9 +12,7 @@
 # > TranslateNodes(n,x,y)
 # > RotateNodes(n,alfa,p)
 # > EllipsisPath(a,b,s)
-# > ShapeArea(nodes,paths) <-- to do
-# > ShapePerimeter(nodes,paths) <-- to do
-# > LineEqFromTwoPoints(p1,p2) <--to do
+# > LineEqFromTwoPoints(p1,p2) 
 
 
 
@@ -27,7 +25,7 @@ def DetMatrix3x3(A,B,C):
            A[2]*(B[0]*C[1]-B[1]*C[0])
            
        
-         
+
 def VertexAngle(a1,a2):
     if a1>a2:
         return a1-a2-(math.pi/2)
@@ -194,16 +192,6 @@ def EllipsisPath(a,b,s):
     return {"nodes":n,"path":p}
 
    
-def ShapeArea(nodes,paths):
-    area=0
-    return area
-
-     
-def ShapePerimeter(nodes,paths):
-    perimeter=0
-    return perimeter
-
-
 def LineEqFromTwoPoints(p1,p2):
     #y = mx + n  ---> Explicit equation of the straight line
     #m = (y1-y2)/(x1-x2)
