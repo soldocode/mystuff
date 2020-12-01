c4d5aa664565fe71f34d6451a09dc6bf50734993
soldocode <riccardo.soldini@gmail.com>
2020-12-01 11:48:51 +0100

    Updated itemToProduct.py

--- scripts/itemToProduct.py
+++ scripts/itemToProduct.py
@@ -1,3 +1,5 @@
+
+
 # -*- coding: utf-8 -*-
 """
 Created on Tue Apr 14 17:42:33 2020
@@ -11,30 +13,22 @@ import datetime
     
 ITEM_FOLDER='\\\\192.168.1.11\Archivio\ARTICOLI'
 ORDER_FOLDER='\\\\192.168.1.11\Archivio\COMMESSE'
-D_YEAR=str(datetime.date.today().year)
 
 
-def itemToProduct(itemPath,orderPath,order,pos,number):
-    print ('itemPath:',itemPath)
-    print ('orderPath:',orderPath)
-    print ('number:',number)
-    msg='Done'
-    if not os.path.exists(itemPath):
-        msg='Errore: '+itemPath+' non trovato!!!'
-    else:
-        if not os.path.exists(orderPath):
-            os.makedirs(orderPath)
-        if os.path.exists(itemPath+'\\LASER'): 
-            OLpath=orderPath+'\\LASER'
-            shutil.copytree(itemPath+'\\LASER', OLpath)
-            thk_folders=os.listdir(OLpath)
-            for f in thk_folders:
-                dxf_files=os.listdir(OLpath+'\\'+f)
-                for d in dxf_files:
-                    if d[-3:].upper()=='DXF':
-                        read_dxf = ezdxf.readfile(OLpath+'\\'+f+'\\'+d)
-                        msp=read_dxf.modelspace()
-                        for e in msp.query('TEXT'):
+
+def copyCuts(orderPath,itemPath,lav,number,order,pos):
+    dest_path=orderPath+'\\'+lav
+    source_path=itemPath+'\\'+lav
+    if not os.path.exists(dest_path):os.makedirs(dest_path)
+    thk_folders=os.listdir(source_path)
+    for f in thk_folders:
+        dxf_files=os.listdir(source_path+'\\'+f)
+        if not os.path.exists(dest_path+'\\'+f):os.makedirs(dest_path+'\\'+f)
+        for d in dxf_files:
+            if d[-3:].upper()=='DXF':
+                read_dxf = ezdxf.readfile(source_path+'\\'+f+'\\'+d)
+                msp=read_dxf.modelspace()
+                for e in msp.query('TEXT'):
                             txt=e.dxf.text
                             spl=txt.split(':')
                             if spl[0]=='PEZZI':
@@ -51,7 +45,26 @@ def itemToProduct(itemPath,orderPath,order,pos,number):
                             if spl[0]=='POS':
                                 txt='POS:'+pos
                                 e.dxf.text=txt
-                        read_dxf.saveas(OLpath+'\\'+f+'\\'+d)                                                            
+                read_dxf.saveas(dest_path+'\\'+f+'\\'+d)
+    return
+
+
+
+def itemToProduct(itemPath,orderPath,order,pos,number):
+    print ('itemPath:',itemPath)
+    print ('orderPath:',orderPath)
+    print ('number:',number)
+    msg='Done'
+    if not os.path.exists(itemPath):
+        msg='Errore: '+itemPath+' non trovato!!!'
+    else:
+        if not os.path.exists(orderPath):
+            os.makedirs(orderPath)
+        if os.path.exists(itemPath+'\\PLASMA'):
+            copyCuts(orderPath,itemPath,'PLASMA',number,order,pos)
+        if os.path.exists(itemPath+'\\LASER'):
+            copyCuts(orderPath,itemPath,'LASER',number,order,pos)
+                                                                       
     return msg
 
 
@@ -67,12 +80,12 @@ if __name__ == "__main__":
     
     customer=input('Cliente: ')
     item=input('Articolo: ')
-    year=input('Anno['+D_YEAR+']: ') or D_YEAR
     order=input('Commessa: ')
     pos=input('Posizione: ')
     num=input('Quantità: ')
+    year=order[0:2]
     
-    order_path=ORDER_FOLDER+'\\'+year+'\\'+year[2:]+order+'\\'+pos
+    order_path=ORDER_FOLDER+'\\20'+year+'\\'+order+'\\'+pos
     item_path=ITEM_FOLDER+'\\'+customer+'\\'+item
         
-    print(itemToProduct(item_path, order_path, year[2:]+order, pos, num))
+    print(itemToProduct(item_path, order_path, order, pos, num))
